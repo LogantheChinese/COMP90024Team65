44370c0fa578a71597f566296fd1a39ce2224293
LogantheChinese <Logan19971224@gmail.com>
2023-05-11 19:02:11 +1000

    5.11 19.02

---
+++ frontend/visualization/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <router-view/>
+</template>
+
+<style lang="less">
+body{
+  background: url("~@/assets/bg.jpg") top center no-repeat;
+}
+*{
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+}
+</style>
---
+++ frontend/visualization/src/views/HomePage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <header>
+        <h1>COVID Discussion In Different Regions With Different Unemployment Rates</h1>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less">
+    header{
+        height: 1rem;
+        width: 100%;
+        background-color: rgba(0, 0, 255, 0.2);
+        h1{
+            font-size: 0.5rem;
+            text-align: center;
+            line-height: 1rem;
+            font-family: 'Times New Roman', Times, serif;
+        }
+    }
+</style>
