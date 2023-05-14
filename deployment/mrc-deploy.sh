4596134c158d7f5886488f72bf9976449db0c9d7
VortexMashiro <mashiro@vortexmashiro.cc>
2023-05-14 10:16:06 +1000

    chore: ansible automation for mrc deployment

--- deployment/mrc-deploy.sh
+++ deployment/mrc-deploy.sh
@@ -5,9 +5,9 @@ func_install_dependencies() {
     echo "Starting to install dependencies..."
     sleep 1
     apt install python3-pip -y
-    pip install ansible openstacksdk
+    pip install "ansible>=2.9" "openstacksdk>=1.0.0"
     ansible --version
-    ansible-galaxy collection install openstack.cloud
+    ansible-galaxy collection install openstack.cloud:2.1.0
 }
 
 func_generate_ssh_key() {
@@ -19,11 +19,29 @@ func_generate_ssh_key() {
         mkdir "$ssh_dir"
     fi
 
-    ssh-keygen -t rsa -b 4096 -f "$ssh_dir/id_rsa" -N ""
+    ssh-keygen -t rsa -b 2048 -f "$ssh_dir/id_rsa" -N ""
 }
 
 func_init_deploy() {
     echo "Starting the initial deployment..."
+
+    read -p "Enter the number of master nodes (default 1): " master_node_count
+    read -p "Enter the number of worker nodes (default 1): " worker_node_count
+    read -p "Enter the instance type (default m3.medium): " node_instance_type
+    read -p "Enter the volume size (default 250G): " node_volume_size
+
+    # If variables are empty, assign default values
+    master_node_count=${master_node_count:-1}
+    worker_node_count=${worker_node_count:-1}
+    node_instance_type=${node_instance_type:-m3.medium}
+    node_volume_size=${node_volume_size:-250}
+
+    # Call the ansible-playbook command with the variables
+    ansible-playbook playbook.yaml \
+      -e master_node_count=$master_node_count \
+      -e worker_node_count=$worker_node_count \
+      -e node_instance_type=$node_instance_type \
+      -e node_volume_size=$node_volume_size
 
 }
 
@@ -37,16 +55,19 @@ func_submit_twitter_files() {
 
 }
 
-# 显示选项并获取用户输入
+# TODO: Work in progress
 echo "Please choose an option:"
-echo "1. Install dependencies"
+echo "--------------------Basic Setup & Tools--------------------"
+echo "1. Install dependencies on deployment host(ansible, openstacksdk, etc.)"
 echo "2. Generate new SSH key pair in ./.ssh/"
-echo "3. Start initial deployment"
-echo "4. Add more nodes to the existing cluster"
-echo "5. Submit new twitter files to the cluster"
+echo "3. Congigure the cluster variables(Default value is 1 Master, 1 Worker, m3.medium, 250G)"
+echo "4. Configure the clouds.yaml with your openstack rc file"
+echo "--------------------Deployment--------------------"
+echo "4. Start initial deployment"
+echo "5. Add more nodes to the existing cluster"
+echo "6. Submit new twitter files to the cluster"
 read -p "Enter your choice: " choice
 
-# 根据用户输入调用相应的函数
 case $choice in
     1) func_install_dependencies ;;
     2) func_generate_ssh_key ;;
